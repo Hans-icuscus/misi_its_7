b619e6863eefaf765cdeb34db783f0b2ad126ed6
Henric Hancus <hanskresna@gmail.com>
2026-02-02 17:22:29 +0700

    Add files via upload

---
+++ FormLaporan.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace AplToko
+{
+    partial class FormLaporan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLaporan));
+            this.label1 = new System.Windows.Forms.Label();
+            this.chartPenjualan = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbTipeLaporan = new System.Windows.Forms.ComboBox();
+            this.btnCetak = new System.Windows.Forms.Button();
+            this.lblTotalSeminggu = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpDari = new System.Windows.Forms.DateTimePicker();
+            this.dtpSampai = new System.Windows.Forms.DateTimePicker();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPenjualan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(281, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Laporan Penjualan";
+            // 
+            // chartPenjualan
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartPenjualan.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartPenjualan.Legends.Add(legend2);
+            this.chartPenjualan.Location = new System.Drawing.Point(26, 153);
+            this.chartPenjualan.Name = "chartPenjualan";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chartPenjualan.Series.Add(series2);
+            this.chartPenjualan.Size = new System.Drawing.Size(684, 307);
+            this.chartPenjualan.TabIndex = 3;
+            this.chartPenjualan.Text = "chart1";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(624, 83);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tipe Laporan";
+            // 
+            // cmbTipeLaporan
+            // 
+            this.cmbTipeLaporan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbTipeLaporan.FormattingEnabled = true;
+            this.cmbTipeLaporan.Items.AddRange(new object[] {
+            "Harian",
+            "Kategori"});
+            this.cmbTipeLaporan.Location = new System.Drawing.Point(171, 83);
+            this.cmbTipeLaporan.Name = "cmbTipeLaporan";
+            this.cmbTipeLaporan.Size = new System.Drawing.Size(74, 21);
+            this.cmbTipeLaporan.TabIndex = 8;
+            this.cmbTipeLaporan.SelectedIndexChanged += new System.EventHandler(this.cmbTipeLaporan_SelectedIndexChanged);
+            // 
+            // btnCetak
+            // 
+            this.btnCetak.Location = new System.Drawing.Point(624, 466);
+            this.btnCetak.Name = "btnCetak";
+            this.btnCetak.Size = new System.Drawing.Size(75, 23);
+            this.btnCetak.TabIndex = 9;
+            this.btnCetak.Text = "Cetak";
+            this.btnCetak.UseVisualStyleBackColor = true;
+            this.btnCetak.Click += new System.EventHandler(this.btnCetak_Click);
+            // 
+            // lblTotalSeminggu
+            // 
+            this.lblTotalSeminggu.AutoSize = true;
+            this.lblTotalSeminggu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalSeminggu.Location = new System.Drawing.Point(56, 476);
+            this.lblTotalSeminggu.Name = "lblTotalSeminggu";
+            this.lblTotalSeminggu.Size = new System.Drawing.Size(231, 17);
+            this.lblTotalSeminggu.TabIndex = 10;
+            this.lblTotalSeminggu.Text = "Total penjualan seminggu terakhir :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.78F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Dari Tanggal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.78F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(367, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Sampai Tanggal";
+            // 
+            // dtpDari
+            // 
+            this.dtpDari.Location = new System.Drawing.Point(152, 118);
+            this.dtpDari.Name = "dtpDari";
+            this.dtpDari.Size = new System.Drawing.Size(172, 20);
+            this.dtpDari.TabIndex = 13;
+            this.dtpDari.ValueChanged += new System.EventHandler(this.dtpDariTanggal_ValueChanged);
+            // 
+            // dtpSampai
+            // 
+            this.dtpSampai.Location = new System.Drawing.Point(484, 118);
+            this.dtpSampai.Name = "dtpSampai";
+            this.dtpSampai.Size = new System.Drawing.Size(171, 20);
+            this.dtpSampai.TabIndex = 14;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // FormLaporan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 513);
+            this.Controls.Add(this.dtpSampai);
+            this.Controls.Add(this.dtpDari);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTotalSeminggu);
+            this.Controls.Add(this.btnCetak);
+            this.Controls.Add(this.cmbTipeLaporan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.chartPenjualan);
+            this.Controls.Add(this.label1);
+            this.Name = "FormLaporan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FormLaporan";
+            this.Load += new System.EventHandler(this.FormLaporan_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartPenjualan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPenjualan;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbTipeLaporan;
+        private System.Windows.Forms.Button btnCetak;
+        private System.Windows.Forms.Label lblTotalSeminggu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpDari;
+        private System.Windows.Forms.DateTimePicker dtpSampai;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
---
+++ FormLaporan.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace AplToko
+{
+    public partial class FormLaporan : Form
+    {
+        public FormLaporan()
+        {
+            InitializeComponent();
+        }
+        private void HitungTotalMingguan()
+        {
+            using (SqlConnection conn = Koneksi.GetConnection())
+            {
+                conn.Open();
+                string query = "SELECT SUM(TotalHarga) FROM Penjualan WHERE Tanggal >= DATEADD(day, -7, GETDATE())";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                var hasil = cmd.ExecuteScalar();
+
+                decimal total = hasil != DBNull.Value ? Convert.ToDecimal(hasil) : 0;
+                lblTotalSeminggu.Text = $"Total penjualan seminggu terakhir: Rp {total:N0}";
+            }
+        }
+        private void TampilkanPenjualanPerHari()
+        {
+            chartPenjualan.Series.Clear();
+            chartPenjualan.ChartAreas.Clear();
+            ChartArea area = new ChartArea("AreaUtama");
+            chartPenjualan.ChartAreas.Add(area);
+            Series series = new Series("Penjualan Per Hari");
+            series.ChartType = SeriesChartType.Column;
+            series.XValueType = ChartValueType.Date;
+            chartPenjualan.Series.Add(series);
+            using (SqlConnection conn = Koneksi.GetConnection())
+            {
+                conn.Open();
+                string query = @"SELECT CAST(Tanggal AS DATE) AS Tgl, SUM(TotalHarga) AS Total 
+                         FROM Penjualan 
+                         WHERE Tanggal BETWEEN @tglAwal AND @tglAkhir
+                         GROUP BY CAST(Tanggal AS DATE) ORDER BY Tgl";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@tglAwal", dtpDari.Value.Date);
+                cmd.Parameters.AddWithValue("@tglAkhir", dtpSampai.Value.Date);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                decimal totalSeluruh = 0;
+
+                while (reader.Read())
+                {
+                    DateTime tanggal = Convert.ToDateTime(reader["Tgl"]);
+                    decimal total = Convert.ToDecimal(reader["Total"]);
+                    series.Points.AddXY(tanggal.ToString("dd-MM"), total);
+                }
+                reader.Close();
+            }
+            chartPenjualan.Titles.Clear();
+            chartPenjualan.Titles.Add("Grafik Penjualan per Hari");
+        }
+
+
+        private void TampilkanPenjualanPerKategori()
+        {
+            chartPenjualan.Series.Clear();
+            chartPenjualan.ChartAreas.Clear();
+            ChartArea area = new ChartArea("AreaKategori");
+            chartPenjualan.ChartAreas.Add(area);
+            Series series = new Series("Penjualan per Kategori");
+            series.ChartType = SeriesChartType.Pie;
+            chartPenjualan.Series.Add(series);
+            using (SqlConnection conn = Koneksi.GetConnection())
+            {
+                conn.Open();
+                string query = @"SELECT k.NamaKategori, SUM(pd.Subtotal) AS Total FROM PenjualanDetail pd JOIN Produk p ON p.Id = pd.ProdukId JOIN Kategori k ON k.Id = p.KategoriId GROUP BY k.NamaKategori";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string kategori = reader["NamaKategori"].ToString();
+                    decimal total = Convert.ToDecimal(reader["Total"]);
+                    series.Points.AddXY(kategori, total);
+                }
+                reader.Close();
+            }
+            chartPenjualan.Titles.Clear();
+            chartPenjualan.Titles.Add("Grafik Penjualan per Kategori");
+        }
+
+        private void FormLaporan_Load(object sender, EventArgs e)
+        {
+            HitungTotalMingguan();
+            TampilkanPenjualanPerKategori();
+        }
+
+
+        private void cmbTipeLaporan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbTipeLaporan.SelectedItem.ToString() == "Harian")
+                TampilkanPenjualanPerHari();
+            else
+                TampilkanPenjualanPerKategori();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            if (cmbTipeLaporan.SelectedItem != null)
+            {
+                string pilihan = cmbTipeLaporan.SelectedItem.ToString();
+
+                if (pilihan == "Harian")
+                {
+                    TampilkanPenjualanPerHari();
+                }
+                else if (pilihan == "Kategori")
+                {
+                    TampilkanPenjualanPerKategori();
+                }
+            }
+
+        }
+
+        private void btnCetak_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.Document = printDocument1;
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void dtpDariTanggal_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap bmp = new Bitmap(this.chartPenjualan.Width, this.chartPenjualan.Height);
+            chartPenjualan.DrawToBitmap(bmp, new Rectangle(0, 0, chartPenjualan.Width, chartPenjualan.Height));
+
+            e.Graphics.DrawString("LAPORAN PENJUALAN", new Font("Arial", 18, FontStyle.Bold), Brushes.Black, new Point(100, 50));
+
+            e.Graphics.DrawImage(bmp, new Point(100, 100));
+        }
+    }
+}
